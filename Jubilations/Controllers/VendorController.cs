7ff38e5fc9d1a2a6dbddf7d597796c2f685d279e
PHkikani <prameshkikani41@gmail.com>
2023-02-09 12:51:16 +0530

    vendor page change

---
+++ Jubilations/Controllers/VendorController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jubilations.Controllers
+{
+    public class VendorController : Controller
+    {
+        // GET: Vendor
+        public ActionResult Vendor()
+        {
+            return View();
+        }
+    }
+}
