c2a7acae214f6efb5030bdf17799eb25b6f9a0a4
Meet Makavana <Meetmakavana6@gmail.com>
2023-02-21 23:57:39 +0530

    A_admin done in mvc

---
+++ Jubilations/Design/A_admin/dist/js/pages/Gallery.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox({
+            alwaysShowClose: true
+        });
+    });
+
+    $('.filter-container').filterizr({ gutterPixels: 3 });
+    $('.btn[data-filter]').on('click', function () {
+        $('.btn[data-filter]').removeClass('active');
+        $(this).addClass('active');
+    });
+})
