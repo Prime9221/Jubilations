4e19482782851594c95725e59526570a081c3af5
DinalBhimani8 <123946143+DinalBhimani8@users.noreply.github.com>
2023-03-11 12:38:45 +0530

    hi

---
+++ Jubilations/Migrations/202303110707061_remove_field_services2.cs
@@ -0,0 +1,20 @@
+﻿namespace Jubilations.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class remove_field_services2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Services", "Services_Status", c => c.String());
+            DropColumn("dbo.Services", "Services_StatusO");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Services", "Services_StatusO", c => c.String());
+            DropColumn("dbo.Services", "Services_Status");
+        }
+    }
+}
