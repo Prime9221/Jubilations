7e929f0cc1dbf9ff7c38e3a87f727487438bb38a
Gopika Rupapara <123942482+342000Gopika@users.noreply.github.com>
2023-03-24 09:44:44 +0530

    logincontroller done

--- Jubilations/Controllers/LoginController.cs
+++ Jubilations/Controllers/LoginController.cs
@@ -1,8 +1,6 @@
 ﻿using Jubilations.Models;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,7 +13,6 @@ namespace Jubilations.Controllers
     {
         DBEntity db = new DBEntity();
         // GET: Login
-        DBEntity db = new DBEntity();
         public ActionResult Login()
         {
             return View();
@@ -124,67 +121,10 @@ namespace Jubilations.Controllers
         {
             return View();
         }
+
         public ActionResult businessForm()
         {
-            return View();
-        }
-
-        //[HttpPost]
-        //public ActionResult businessForm(Register reg)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        Register a = new Register();
-
-        //        a.Owner_Name = reg.Owner_Name;
-        //        a.Business_Name = reg.Business_Name;
-        //        a.Business_Addres = reg.Business_Addres;
-        //        a.GST_NO = reg.GST_NO;
-        //        a.Category = reg.Category;
-        //        a.City = reg.City;
-        //        a.Email_ID = reg.Email_ID;
-        //        a.Phone_No = reg.Phone_No;
-        //        a.Password = reg.Password;
-        //        a.Confirm_Password = reg.Confirm_Password;
-        //        a.Create_Date = DateTime.Now.ToShortDateString();
-        //        //a.Category_Update_Date = DateTime.Now.ToShortDateString();
-        //        db.registers.Add(a);
-        //        db.SaveChanges();
-        //        TempData["DataInserted"] = "true";
-        //        return RedirectToAction("businessForm");
-
-        //    }
-        //    return RedirectToAction("businessForm");
-        //}
-
-        [HttpPost]
-        public ActionResult businessForm(User e)
-        {
-            if (Request.HttpMethod == "POST")
-            {
-                User er = new User();
-                using (SqlConnection con = new SqlConnection("Data Source=192.168.29.252,1434;Integrated Security=false;Initial Catalog=Jubilation; User Id=sa;password=Makm9221@"))
-                {
-                    using (SqlCommand cmd = new SqlCommand("SP_EnrollDetail"))
-                    {
-                        cmd.CommandType = CommandType.StoredProcedure;
-                        cmd.Parameters.AddWithValue("@Owner Name", e.User_Name);
-                        cmd.Parameters.AddWithValue("@Business Name", e.User_ShopName);
-                        cmd.Parameters.AddWithValue("@Business Address", e.User_Address);
-                        cmd.Parameters.AddWithValue("@GST NO", e.User_GSTNO);
-                        cmd.Parameters.AddWithValue("@Category", e.User_DOB);
-                        cmd.Parameters.AddWithValue("@City", e.User_Profile_Image);
-                        cmd.Parameters.AddWithValue("@Email ID", e.User_Email);
-                        cmd.Parameters.AddWithValue("@Phone No", e.User_PhoneNo);
-                        cmd.Parameters.AddWithValue("@Password", e.User_Password );
-                        cmd.Parameters.AddWithValue("@User_Name", "INSERT");
-                        con.Open();
-                        ViewData["result"] = cmd.ExecuteNonQuery();
-                        con.Close();
-                    }
-                }
-            }
-            return View();
+            return View("businessForm");
         }
         [HttpPost]
         public ActionResult businessForm(User model)
@@ -196,7 +136,7 @@ namespace Jubilations.Controllers
                 User a = new User();
 
                 a.User_Name = model.User_Name;
-                a.User_ShopName= model.User_ShopName;
+                a.User_ShopName = model.User_ShopName;
                 a.User_Address = model.User_Address;
                 a.User_GSTNO = model.User_GSTNO;
                 a.User_Email = model.User_Email;
@@ -209,7 +149,9 @@ namespace Jubilations.Controllers
                 TempData["DataInserted"] = "true";
                 return RedirectToAction("Login");
 
-
+            }
+            return RedirectToAction("Login");
+        }
         public ActionResult verifyotp()
         {
             return View();
